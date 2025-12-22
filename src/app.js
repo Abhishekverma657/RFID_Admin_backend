0b1981554d854a5934b3c3fda4aa41071ef2096f
Abhishek Verma <vermabhishek657@gmail.com>
2025-12-22 22:23:47 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors(
     {
-        origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"], // Explicitly allow frontend origins
+        origin: ["http://localhost:5173","http://109.106.255.78:5500/", "http://109.106.255.78:5500", "http://localhost:5174", "http://localhost:3000"], // Explicitly allow frontend origins
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         credentials: true,
     }
